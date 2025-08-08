df39248fd260579b0be1dfe21db07d44d971dbe7
Anori_2 <abuamer04@gmail.com>
2025-08-08 23:52:27 +0200

    refactor: reset to Angular starter page and remove unused files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { HumanmeterComponent } from './humanmeter/humanmeter.component';
-import { A11yModule, LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [HumanmeterComponent, A11yModule],
-  providers: [LiveAnnouncer],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('humanmeter');
+  title = 'humanmeter';
 }
